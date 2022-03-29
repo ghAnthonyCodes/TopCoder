fe229b8a32f7e72909e8f11c9e8b3e18ceb926fa
Anthony Meyer <85413356+ghAnthonyCodes@users.noreply.github.com>
2022-03-29 14:08:38 -0400

    Update VisitPoints.cpp

--- SRM 826/VisitPoints.cpp
+++ SRM 826/VisitPoints.cpp
@@ -4,29 +4,29 @@
 class VisitPoints {
   public:
     std::string visit(std::vector<int> X, std::vector<int> Y) {
-        int maxX = 0;
-        int maxY = 0;
-        std::string ans = "";
-        for (int i = 0; i < X.size(); i++) {
-        	maxX = std::max(maxX, X[i]);
-            maxY = std::max(maxY, Y[i]);
-        }
-        bool goingUp = true;
-        for (int i=0; i <= maxX; i++) {
-            if (goingUp) {
-                for (int j=0; j < maxY; j++) 
-                	ans += "N";
-                if (i != maxX)
-                    ans += "E";
-                goingUp = false;
-            } else {
-                for (int j=0; j < maxY; j++)
-                    ans += "S";
-                if (i != maxX)
-                    ans += "E";
-                goingUp = true;
-            }
-        }
-     	return ans;   
+    int maxX = 0;
+    int maxY = 0;
+    std::string ans = "";
+    for (int i = 0; i < X.size(); i++) {
+      maxX = std::max(maxX, X[i]);
+      maxY = std::max(maxY, Y[i]);
+    }
+    bool goingUp = true;
+    for (int i=0; i <= maxX; i++) {
+      if (goingUp) {
+        for (int j=0; j < maxY; j++) 
+          ans += "N";
+        if (i != maxX)
+          ans += "E";
+        goingUp = false;
+      } else {
+        for (int j=0; j < maxY; j++)
+          ans += "S";
+        if (i != maxX)
+          ans += "E";
+        goingUp = true;
+      }
+    }
+    return ans;   
     }
 };
