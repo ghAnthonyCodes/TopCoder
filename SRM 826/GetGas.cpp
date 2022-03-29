92ab431a0668c44fc1d6a160af0a1703bbe6f13e
Anthony Meyer <85413356+ghAnthonyCodes@users.noreply.github.com>
2022-03-29 14:06:40 -0400

    Update GetGas.cpp

--- SRM 826/GetGas.cpp
+++ SRM 826/GetGas.cpp
@@ -1,21 +1,18 @@
 #include <vector>
+#include <limits>
 class GetGas {
   public:
     double optimize(int G, std::vector<int> Dto, std::vector<int> Dfrom, std::vector<int> gasPrice) {
         
         // Calc cost
-        double minPrice = 0;
+        double minPrice = std::numeric_limits<double>::infinity();
         for (int i = 0; i < Dto.size(); i++) {
             
             double gallonsLeft = G - Dto[i]/25.0;
             if (gallonsLeft < 0)
                 continue;
             double cost = gasPrice[i]*(Dfrom[i] - gallonsLeft*25.0)/25.0;
-
-            if (i == 0)
-                minPrice = std::max(0.0, cost);
-            else
-                minPrice = std::max(0.0, std::min(cost, minPrice));
+			minPrice = std::max(0.0, std::min(cost, minPrice));
         }
         return minPrice;   
     }
