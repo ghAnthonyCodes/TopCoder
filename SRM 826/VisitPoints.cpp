e5e9e5c781ab93b91b098cb3bb4aeee3cf7c76a0
Anthony Meyer <85413356+ghAnthonyCodes@users.noreply.github.com>
2022-03-29 13:54:41 -0400

    Create VisitPoints.cpp

---
+++ SRM 826/VisitPoints.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+
+class VisitPoints {
+  public:
+    std::string visit(std::vector<int> X, std::vector<int> Y) {
+        int maxX = 0;
+        int maxY = 0;
+        std::string ans = "";
+        for (int i = 0; i < X.size(); i++) {
+        	maxX = std::max(maxX, X[i]);
+            maxY = std::max(maxY, Y[i]);
+        }
+        bool goingUp = true;
+        for (int i=0; i <= maxX; i++) {
+            if (goingUp) {
+                for (int j=0; j < maxY; j++) 
+                	ans += "N";
+                if (i != maxX)
+                    ans += "E";
+                goingUp = false;
+            } else {
+                for (int j=0; j < maxY; j++)
+                    ans += "S";
+                if (i != maxX)
+                    ans += "E";
+                goingUp = true;
+            }
+        }
+     	return ans;   
+    }
+};
